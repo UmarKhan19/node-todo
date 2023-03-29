c36139f3a390fa917e6bb0b81e60dd29d0899b94
UmarKhan19 <khanumar0108@gmail.com>
2023-03-29 22:36:53 +0530

    add mongo atlas

--- data/database.js
+++ data/database.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = () => {
   mongoose
     .connect(process.env.MONGO_URI, { dbName: "backendAPI" })
-    .then(() => {
-      console.log("connected to the database");
+    .then((c) => {
+      console.log(`connected to the database on ${c.connection.host}`);
     })
     .catch((e) => {
       console.log(e);
